cb7060a997728a55155e9887f720265e17aa603e
Pietro Cattaneo <pieca314@gmail.com>
2018-12-03 12:19:17 +0100

    Fix Iteratori

--- Lab_TAP_4/MacroClass.cs
+++ Lab_TAP_4/MacroClass.cs
@@ -4,48 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_TAP_4
-{
-    public class MacroClass
-    {
-        public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, T value, IEnumerable<T> newValues)
-        {
-            if(sequence == null)
+namespace Lab_TAP_4 {
+    public class MacroClass {
+        public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, T value, IEnumerable<T> newValues) {
+            if (sequence == null)
                 throw new ArgumentNullException(nameof(sequence));
-            if(newValues == null)
+            if (newValues == null)
                 throw new ArgumentNullException(nameof(newValues));
 
-            if(!sequence.Contains(value))
+            if (!sequence.Contains(value))
                 return sequence;
 
-            List<T> newList = new List<T>();
-          /*Con Iterators
-            IEnumerator<T> _sequeEnumerator = sequence.GetEnumerator();
+            var newSequence = new List<T>();
 
-            while (_sequeEnumerator.MoveNext())
-            {
-                if (_sequeEnumerator.Current.Equals(value))
-                {            
-                IEnumerator<T> __newValEnum = newValues.GetEnumerator();
+            var sequenceEnumerator = sequence.GetEnumerator();
+            var __newValEnum = newValues.GetEnumerator();
 
-                    while (__newValEnum.MoveNext())
-                    {
-                        newList.Add(__newValEnum.Current);
+            while (sequenceEnumerator.MoveNext()) {
+                if (!sequenceEnumerator.Current.Equals(value)) {
+                    newSequence.Add(sequenceEnumerator.Current);
+                }
+                else {
+                    while (__newValEnum.MoveNext()) {
+                        newSequence.Add(__newValEnum.Current);
                     }
-                   //_newVaEnum.reset();
-                }  
-               else newList.Add(_sequeEnumerator.Current);
+                    __newValEnum.Reset();
+                }
             }
 
-    */
-            foreach (var s in sequence)
-            {
-                if(s.Equals(value))
-                    newList.AddRange(newValues);
-                else newList.Add(s);
-            }
-
-            return newList;
+            return newSequence;
         }
 
     }
--- lab_TAP_4.Tests/MacroTests.cs
+++ lab_TAP_4.Tests/MacroTests.cs
@@ -2,11 +2,6 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Castle.Core.Internal;
-using Moq;
 
 namespace lab_TAP_4.Tests
 {
@@ -14,13 +9,15 @@ namespace lab_TAP_4.Tests
     public class MacroTests
     {
         [Test]
+        [Category("Exceptions")]
         public void Sequence_Null_ThrowsException()
         {
             Assert.That(() => MacroClass.MacroExpansion(null, 1, null), Throws.TypeOf<ArgumentNullException>());
         }
 
         [Test]
-        public void newValues_Null_ThrowsException()
+        [Category("Exceptions")]
+        public void NewValues_Null_ThrowsException()
         {
             Assert.That(() => MacroClass.MacroExpansion(null, 1, null), Throws.TypeOf<ArgumentNullException>());
         }
@@ -43,10 +40,10 @@ namespace lab_TAP_4.Tests
         [TestCase(new[] {1, 2, 1, 2, 3}, 6, new[] {7, 8, 9})]
         [TestCase(new[] {1, 2, 1, 2, 3}, 7, new[] {7, 8, 9})]
         [TestCase(new[] {1, 2, 1, 2, 3}, 8, new[] {7, 8, 9})]
-        public void StringValueNotInSequence_ReturnsSequence(int[]_sequence, int _value, int[] _newSequence)
+        public void StringValueNotInSequence_ReturnsSequence(int[]sequence, int value, int[] newSequence)
         {
-            var res = MacroClass.MacroExpansion(_sequence, _value, _newSequence);
-            Assert.That(res, Is.EqualTo(_sequence));
+            var res = MacroClass.MacroExpansion(sequence, value, newSequence);
+            Assert.That(res, Is.EqualTo(sequence));
         }
 
         [Test]
@@ -59,10 +56,10 @@ namespace lab_TAP_4.Tests
         [TestCase(new[] {5, 2, 1, 2, 3}, 5, new[] {7, 8, 9}, new[] {7, 8, 9, 2, 1, 2, 3})]
         [TestCase(new[] {1, 2, 5, 2, 3}, 5, new[] {7, 8, 9}, new[] {1, 2, 7, 8, 9, 2, 3})]
         [TestCase(new[] {1, 2, 1, 2, 5}, 5, new[] {7, 8, 9}, new[] {1, 2, 1, 2, 7, 8, 9})]
-        public void OneValueOccurence(int[] _sequence, int _value, int[] _newSequence, int[] _res)
+        public void OneValueOccurence(int[] sequence, int value, int[] newSequence, int[] resultSequence)
         {
-            var res = MacroClass.MacroExpansion(_sequence, _value, _newSequence);
-            Assert.That(res, Is.EqualTo(_res));
+            var res = MacroClass.MacroExpansion(sequence, value, newSequence);
+            Assert.That(res, Is.EqualTo(resultSequence));
         }
     }
 }
