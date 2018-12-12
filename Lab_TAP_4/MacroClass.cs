c0307e6534ec023cbf660a2f790cdd63f04640fc
Pietro Cattaneo <pieca314@gmail.com>
2018-12-12 16:19:05 +0100

    More Tests

--- Lab_TAP_4/MacroClass.cs
+++ Lab_TAP_4/MacroClass.cs
@@ -4,37 +4,42 @@ using System.Linq;
 
 namespace Lab_TAP_4 {
     public class MacroClass {
-        public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, T value, IEnumerable<T> newValues) {
-            if (sequence == null)
-                throw new ArgumentNullException(nameof(sequence));
-            if (newValues == null)
-                throw new ArgumentNullException(nameof(newValues));
+        public static ICollection<T> MacroExpansion<T>(ICollection<T> sequence, T value, ICollection<T> newValues) {
+
+            CheckNotNull(sequence, newValues);
 
             if (!sequence.Contains(value))
                 return sequence;
 
             var newSequence = new List<T>();
 
-            var sequenceEnumerator = sequence.GetEnumerator();
-            var __newValEnum = newValues.GetEnumerator();
+            using (var sequenceEnumerator = sequence.GetEnumerator())
+            using (var newValEnum = newValues.GetEnumerator()) {
 
-            while (sequenceEnumerator.MoveNext()) {
-                if (!sequenceEnumerator.Current.Equals(value)) {
-                    newSequence.Add(sequenceEnumerator.Current);
-                }
-                else {
-                    while (__newValEnum.MoveNext()) {
-                        newSequence.Add(__newValEnum.Current);
+                while (sequenceEnumerator.MoveNext()) {
+                    if (!Equals(sequenceEnumerator.Current, value)) {
+                        newSequence.Add(sequenceEnumerator.Current);
                     }
-                    __newValEnum.Reset();
+                    else {
+                        //Potrei usare List.AddRange(IEnumerable<T>)
+                        while (newValEnum.MoveNext()) {
+                            newSequence.Add(newValEnum.Current);
+                        }
+                        newValEnum.Reset();
+                    }
                 }
             }
-
             return newSequence;
         }
 
-        public static void Main()
-        {
+        private static void CheckNotNull<T>(ICollection<T> sequence, ICollection<T> newValues) {
+            if (sequence == null)
+                throw new ArgumentNullException(nameof(sequence));
+            if (newValues == null)
+                throw new ArgumentNullException(nameof(newValues));
+        }
+
+        public static void Main() {
             MacroExpansion(new[] { 1, 2, 1, 2, 3 }, 2, new[] { 7, 8, 9 })
                 .ToList()
                 .ForEach(Console.Write);
--- lab_TAP_4.Tests/MacroTests.cs
+++ lab_TAP_4.Tests/MacroTests.cs
@@ -1,57 +1,90 @@
-﻿using Lab_TAP_4;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 
-namespace lab_TAP_4.Tests {
+namespace Lab_TAP_4.Tests {
     [TestFixture]
     public class MacroTests {
+
         [Test]
         [Category("Exceptions")]
         public void Sequence_Null_ThrowsException() {
-            Assert.That(() => MacroClass.MacroExpansion(null, 1, null),
+            Assert.That(() => MacroClass.MacroExpansion(null, 1, new[] { 1, 2 }),
                 Throws.TypeOf<ArgumentNullException>());
         }
 
         [Test]
         [Category("Exceptions")]
         public void NewValues_Null_ThrowsException() {
-            Assert.That(() => MacroClass.MacroExpansion(null, 1, null),
+            Assert.That(() => MacroClass.MacroExpansion(new[] { 1, 2 }, 1, null),
                 Throws.TypeOf<ArgumentNullException>());
         }
 
         [Test]
         [Category("Exceptions")]
+        public void Sequences_Null_ThrowsException() {
+            Assert.That(() => MacroClass.MacroExpansion(null, 1, null),
+                Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Category("Exceptions")]
         [TestCase(null, 1, null)]
-        public void NullParameters_ThrowsException(IEnumerable<int> sequence, int value, IEnumerable<int> newValues) {
-            Assert.That(() => MacroClass.MacroExpansion(sequence, value, newValues), Throws.TypeOf<ArgumentNullException>());
+        [TestCase(null, 1, new[] { 1, 2 })]
+        [TestCase(new[] { 1, 2 }, 1, null)]
+        public void NullParameters_ThrowsException(ICollection<int> sequence, int value, ICollection<int> newValues) {
+            Assert.That(() => MacroClass.MacroExpansion(sequence, value, newValues), 
+                Throws.TypeOf<ArgumentNullException>());
         }
 
         [Test]
+        [Category("Correct cases")]
         public void IntValueNotInSequence_ReturnsSequence() {
-            IEnumerable<int> res = MacroClass.MacroExpansion(new[] { 1, 2, 1, 2, 3 }, 5, new[] { 7, 8, 9 });
+            var res = MacroClass.MacroExpansion(new[] { 1, 2, 1, 2, 3 }, 5, new[] { 7, 8, 9 });
             Assert.That(res, Is.EqualTo(new[] { 1, 2, 1, 2, 3 }));
         }
 
         [Test]
+        [Category("Correct cases")]
+        public void IntValueNotInSequence_ReturnsSequenceWithoutNewValue() {
+            var res = MacroClass.MacroExpansion(new[] { 1, 2, 1, 2, 3 }, 5, new[] { 7 });
+            Assert.That(res, !Contains.Item(7));
+        }
+
+        [Test]
+        [Category("Correct cases")]
         public void StringValueNotInSequence_ReturnsSequence() {
-            IEnumerable<string> res = MacroClass.MacroExpansion(new[] { "a", "b", "c" }, "f", new[] { "S" });
-            Assert.That(res, Is.EqualTo(new[] { "a", "b", "c" }));
+            var res = MacroClass.MacroExpansion(new[] { "s1", "s2", "s3", "s4" }, "s5", new[] { "s1" });
+            Assert.That(res, Is.EqualTo(new[] { "s1", "s2", "s3", "s4" }));
+        }
+
+        [Test]
+        public void StringValueNotInSequence_ReturnsSequenceWithoutNewValue() {
+            var res = MacroClass.MacroExpansion(new[] { "a", "b", "c" }, "f", new[] { "S" });
+            Assert.That(res, !Contains.Item("S"));
         }
 
         [TestCase(new[] { 1, 2, 1, 2, 3 }, 5, new[] { 7, 8, 9 })]
-        [TestCase(new[] { 1, 2, 1, 2, 3 }, 6, new[] { 7, 8, 9 })]
-        [TestCase(new[] { 1, 2, 1, 2, 3 }, 7, new[] { 7, 8, 9 })]
-        [TestCase(new[] { 1, 2, 1, 2, 3 }, 8, new[] { 7, 8, 9 })]
-        public void StringValueNotInSequence_ReturnsSequence(int[] sequence, int value, int[] newSequence) {
+        public void IntValueNotInSequence_ReturnsSequence(int[] sequence, int value, int[] newSequence) {
             var res = MacroClass.MacroExpansion(sequence, value, newSequence);
             Assert.That(res, Is.EqualTo(sequence));
         }
 
         [Test]
         public void OneValueOccurrence_Head() {
-            IEnumerable<int> res = MacroClass.MacroExpansion(new[] { 3, 2, 1, 2, 1 }, 3, new[] { 7, 8, 9 });
+            var res = MacroClass.MacroExpansion(new[] { 3, 2, 1, 2, 1 }, 3, new[] { 7, 8, 9 });
             Assert.That(res, Is.EqualTo(new[] { 7, 8, 9, 2, 1, 2, 1 }));
+        }
+
+        [Test]
+        public void OneValueOccurrence_Middle() {
+            var res = MacroClass.MacroExpansion(new[] { 1, 2, 3, 2, 1 }, 3, new[] { 7, 8, 9 });
+            Assert.That(res, Is.EqualTo(new[] { 1, 2, 7, 8, 9, 2, 1 }));
+        }
+
+        [Test]
+        public void OneValueOccurrence_Tail() {
+            var res = MacroClass.MacroExpansion(new[] { 1, 2, 1, 2, 3 }, 3, new[] { 7, 8, 9 });
+            Assert.That(res, Is.EqualTo(new[] { 1, 2, 1, 2, 7, 8, 9  }));
         }
 
         [TestCase(new[] { 5, 2, 1, 2, 3 }, 5, new[] { 7, 8, 9 }, new[] { 7, 8, 9, 2, 1, 2, 3 })]
